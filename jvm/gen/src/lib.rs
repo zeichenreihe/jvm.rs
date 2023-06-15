dde99cad755b7d4d1b3e7656218f871b39e6d1b3
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-06-16 01:56:36 +0200

    almost finish the class reader, besides the constant pool
    lib.rs
    - rename `Decl` to `Declaration` (well still not best)
    - get rid of zerocopy, don't use C + packed anymore!
    - use `Vec::width_capacity`
    - implement Load for these types
    - add a `type_width` field for storing the original width in bytes
    
    classfile.rs:
    - use rusts struct name convention
    - actually make a test case
    - define some Error and Load types

--- jvm/gen/src/lib.rs
+++ jvm/gen/src/lib.rs
@@ -2,27 +2,35 @@ use proc_macro::TokenStream;
 
 use quote::quote;
 use syn::{braced, bracketed, Expr, Ident, parse_macro_input, Result, token, Token};
+use syn::__private::TokenStream2;
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 
 struct Typed {
-	typ: Ident,
+	type_width: Option<usize>,
+	typ: TokenStream2,
 	name: Ident,
 	squares: Option<Expr>
 }
 
 impl Parse for Typed {
 	fn parse( input: ParseStream ) -> Result<Self> {
-		let mut typ: Ident = input.parse()?;
-		typ = Ident::new(
-			match typ.to_string().as_str() {
-				"u1" => "u8",
-				"u2" => "u16",
-				"u4" => "u32",
-				i => i,
-			},
-			typ.span()
-		);
+		let typ: Ident = input.parse()?;
+
+		let type_width = match typ.to_string().as_str() {
+			"u1" => Some(1),
+			"u2" => Some(2),
+			"u4" => Some(4),
+			_ => None,
+		};
+
+		let typ: TokenStream2 = match typ.to_string().as_str() {
+			"u1" => "u8",
+			"u2" => "u16",
+			"u4" => "u32",
+			i => i,
+		}.parse()?;
+
 		let name = input.parse()?;
 
 		let squares = if input.peek( token::Bracket ) {
@@ -33,45 +41,93 @@ impl Parse for Typed {
 			None
 		};
 
-		Ok( Typed { typ, name, squares } )
+		Ok( Typed { type_width, typ, name, squares } )
 	}
 }
 
-struct Decl {
+struct Declaration {
 	name: Ident,
 	fields: Punctuated<Typed, Token![;]>,
 }
 
-impl Parse for Decl {
+impl Parse for Declaration {
 	fn parse( input: ParseStream ) -> Result<Self> {
 		let name = input.parse()?;
 		let content;
 		braced!( content in input );
 		let fields = content.parse_terminated( Typed::parse, Token![;] )?;
 
-		Ok( Decl { name, fields } )
+		Ok( Self { name, fields } )
 	}
 }
 
 #[proc_macro]
 pub fn declare_jvm_struct(tokens: TokenStream ) -> TokenStream {
-	let input = parse_macro_input!( tokens as Decl );
+	let input = parse_macro_input!( tokens as Declaration );
 
 	let name = input.name;
 
-	let mut fields = Vec::new();
+	let mut fields = Vec::with_capacity(input.fields.len());
+	let mut load = Vec::with_capacity(input.fields.len());
+	let mut load_struct = Vec::with_capacity(input.fields.len());
 	for field in input.fields {
 		let typ = field.typ;
 		let name = field.name;
 		let squares = field.squares;
-		fields.push( quote! { pub #name: #typ, } );
+
+		fields.push( if let Some(_) = field.type_width {
+			quote! {
+				pub #name: #typ,
+			}
+		} else {
+			quote! {
+				pub #name: Vec<#typ>,
+			}
+		});
+
+		load.push(if let Some(n) = field.type_width {
+			quote! {
+				let #name = {
+					let mut buf = [0u8; #n];
+					let length = reader.read(&mut buf)?;
+					if length == #n {
+						#typ::from_be_bytes(buf)
+					} else {
+						Err(Error::ReadWrongAmountOfData { actual: length, expected: #n, string: stringify!(#name)})?
+					}
+				};
+			}
+		} else {
+			quote! {
+				let #name = {
+					let mut vec = Vec::with_capacity((#squares) as usize);
+					for _ in 0..((#squares) as usize) {
+						vec.push(#typ::load(reader)?);
+					}
+					vec
+				};
+			}
+		});
+		load_struct.push(
+			quote! {
+				#name,
+			}
+		)
 	}
 
 	let exp = quote! {
-		#[repr(C, packed)]
-		#[derive(Debug, AsBytes, FromBytes)]
+		#[derive(Debug)]
 		struct #name {
 			#( #fields )*
+		}
+
+		impl Load for #name {
+			fn load<R: Read>(reader: &mut R) -> Result<#name, Error> {
+				#( #load )*
+				Ok( Self {
+					#( #load_struct )*
+				} )
+			}
 		}
 	};
 
--- jvm/src/classfile.rs
+++ jvm/src/classfile.rs
@@ -1,26 +1,42 @@
-use zerocopy::{AsBytes, FromBytes};
+use std::io::Read;
 
 use gen::declare_jvm_struct;
 
+#[derive(Debug)]
+enum Error {
+	ReadWrongAmountOfData{ actual: usize, expected: usize, string: &'static str},
+	IoError(std::io::Error)
+}
+
+impl From<std::io::Error> for Error {
+	fn from(value: std::io::Error) -> Self {
+		Self::IoError(value)
+	}
+}
+
+trait Load {
+	fn load<R: Read>(reader: &mut R) -> Result<Self, Error> where Self: Sized;
+}
+
 declare_jvm_struct!(
-	cp_info {
+	CpInfo {
 	    u1 tag;
 	    u1 info[];
 	}
 );
 
 declare_jvm_struct!(
-	field_info {
+	FieldInfo {
 	    u2             access_flags;
 	    u2             name_index;
 	    u2             descriptor_index;
 	    u2             attributes_count;
-	    attribute_info attributes[attributes_count];
+	    AttributeInfo  attributes[attributes_count];
 	}
 );
 
 declare_jvm_struct!(
-	attribute_info {
+	AttributeInfo {
 	    u2 attribute_name_index;
 	    u4 attribute_length;
 	    u1 info[attribute_length];
@@ -28,12 +44,12 @@ declare_jvm_struct!(
 );
 
 declare_jvm_struct!(
-	method_info {
+	MethodInfo {
 	    u2             access_flags;
 	    u2             name_index;
 	    u2             descriptor_index;
 	    u2             attributes_count;
-	    attribute_info attributes[attributes_count];
+	    AttributeInfo  attributes[attributes_count];
 	}
 );
 
@@ -43,29 +59,28 @@ declare_jvm_struct!(
 	    u2             minor_version;
 	    u2             major_version;
 	    u2             constant_pool_count;
-	    cp_info        constant_pool[constant_pool_count-1];
+	    CpInfo         constant_pool[constant_pool_count-1];
 	    u2             access_flags;
 	    u2             this_class;
 	    u2             super_class;
 	    u2             interfaces_count;
 	    u2             interfaces[interfaces_count];
 	    u2             fields_count;
-	    field_info     fields[fields_count];
+	    FieldInfo      fields[fields_count];
 	    u2             methods_count;
-	    method_info    methods[methods_count];
+	    MethodInfo     methods[methods_count];
 	    u2             attributes_count;
-	    attribute_info attributes[attributes_count];
+	    AttributeInfo  attributes[attributes_count];
 	}
 );
 
-impl ClassFile {
-
-}
-
 #[cfg(test)]
 mod testing {
+	use super::{ClassFile, Load};
 	#[test]
 	fn try_parse_classfile() {
-
+		let bytes = include_bytes!("../../java_example_classfiles/Test.java");
+		let classfile = ClassFile::load(&mut &bytes[..]).unwrap();
+		dbg!(classfile);
 	}
 }
