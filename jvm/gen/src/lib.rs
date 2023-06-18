aa555bf23de35c910aba49f9a4bb9a6c40638ef8
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-06-18 21:59:14 +0200

    make the classfile struct/parser generator work for all things

--- jvm/gen/src/lib.rs
+++ jvm/gen/src/lib.rs
@@ -1,135 +1,259 @@
-use proc_macro::TokenStream;
-
+use proc_macro::TokenStream as TokenStream1;
 use quote::quote;
-use syn::{braced, bracketed, Expr, Ident, parse_macro_input, Result, token, Token};
-use syn::__private::TokenStream2;
+use syn::{braced, bracketed, Expr, ExprLit, Ident, parenthesized, parse_macro_input, Result, token, Token};
+use proc_macro2::TokenStream;
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
+use syn::token::{Enum, Struct};
 
-struct Typed {
-	type_width: Option<usize>,
-	typ: TokenStream2,
-	name: Ident,
-	squares: Option<Expr>
-}
+#[derive(Debug)]
+struct SpecialType(TokenStream);
 
-impl Parse for Typed {
-	fn parse( input: ParseStream ) -> Result<Self> {
+impl Parse for SpecialType {
+	fn parse(input: ParseStream) -> Result<Self> {
 		let typ: Ident = input.parse()?;
-
-		let type_width = match typ.to_string().as_str() {
-			"u1" => Some(1),
-			"u2" => Some(2),
-			"u4" => Some(4),
-			_ => None,
-		};
-
-		let typ: TokenStream2 = match typ.to_string().as_str() {
-			"u1" => "u8",
-			"u2" => "u16",
-			"u4" => "u32",
-			i => i,
-		}.parse()?;
-
-		let name = input.parse()?;
-
-		let squares = if input.peek( token::Bracket ) {
-			let content;
-			bracketed!( content in input );
-			content.call( Expr::parse ).map_or( None, |x| Some(x) )
-		} else {
-			None
-		};
-
-		Ok( Typed { type_width, typ, name, squares } )
+		Ok(SpecialType(match typ.to_string().as_str() {
+				"u1" => "u8",
+				"u2" => "u16",
+				"u4" => "u32",
+				i => i,
+			}.parse()?
+		))
 	}
 }
 
-struct Declaration {
+struct Typed {
+	typ: SpecialType,
+	name: Ident,
+	squares: Option<Expr>,
+}
+
+impl Parse for Typed {
+	fn parse(input: ParseStream) -> Result<Self> {
+		Ok(Typed {
+			typ: SpecialType::parse(input)?,
+			name: input.parse()?,
+			squares: if input.peek( token::Bracket ) {
+				let content;
+				bracketed!( content in input );
+				Some(content.call( Expr::parse )?)
+			} else {
+				None
+			},
+		})
+	}
+}
+
+struct EnumTyped {
+	index: ExprLit,
 	name: Ident,
 	fields: Punctuated<Typed, Token![;]>,
 }
 
-impl Parse for Declaration {
-	fn parse( input: ParseStream ) -> Result<Self> {
-		let name = input.parse()?;
-		let content;
-		braced!( content in input );
-		let fields = content.parse_terminated( Typed::parse, Token![;] )?;
+impl Parse for EnumTyped {
+	fn parse(input: ParseStream) -> Result<Self> {
+		let index = input.parse()?;
+		input.parse::<Token![=]>()?;
+		Ok(EnumTyped {
+			index,
+			name: input.parse()?,
+			fields: {
+				let content;
+				braced!(content in input);
+				content.parse_terminated( Typed::parse, Token![;])?
+			},
+		})
+	}
+}
 
-		Ok( Self { name, fields } )
+enum Declaration {
+	Struct {
+		name: Ident,
+		fields: Punctuated<Typed, Token![;]>,
+		braces: Option<Expr>,
+	},
+	Enum {
+		name: Ident,
+		tag_type: SpecialType,
+		variants: Punctuated<EnumTyped, Token![,]>,
+	}
+}
+
+impl Parse for Declaration {
+	fn parse(input: ParseStream) -> Result<Self> {
+		let lookahead = input.lookahead1();
+		if lookahead.peek(Enum) {
+			input.parse::<Token![enum]>()?;
+			Ok(Self::Enum {
+				name: input.parse()?,
+				tag_type: {
+					let content;
+					parenthesized!(content in input);
+					SpecialType::parse(&content)?
+				},
+				variants: {
+					let content;
+					braced!(content in input);
+					content.parse_terminated(EnumTyped::parse, Token![,])?
+				},
+			})
+		} else if lookahead.peek(Struct) {
+			input.parse::<Token![struct]>()?;
+			Ok(Self::Struct {
+				name: input.parse()?,
+				fields: {
+					let content;
+					braced!( content in input );
+					content.parse_terminated( Typed::parse, Token![;])?
+				},
+				braces: if input.peek(token::Brace) {
+					let content;
+					braced!(content in input);
+					Some(content.call(Expr::parse)?)
+				} else {
+					None
+				},
+			})
+		} else {
+			Err(lookahead.error())?
+		}
 	}
 }
 
 #[proc_macro]
-pub fn declare_jvm_struct(tokens: TokenStream ) -> TokenStream {
+pub fn declare_jvm_struct(tokens: TokenStream1) -> TokenStream1 {
 	let input = parse_macro_input!( tokens as Declaration );
+	match input {
+		Declaration::Struct { name, fields, braces} => {
+			let mut field_declarations = Vec::with_capacity(fields.len());
+			let mut load = Vec::with_capacity(fields.len());
+			let mut load_struct = Vec::with_capacity(fields.len());
+			for field in fields {
+				let typ = field.typ.0;
+				let name = field.name;
 
-	let name = input.name;
+				field_declarations.push( if field.squares.is_some() { quote! {
+					pub #name: Vec<#typ>,
+				}} else { quote!{
+					pub #name: #typ,
+				}});
 
-	let mut fields = Vec::with_capacity(input.fields.len());
-	let mut load = Vec::with_capacity(input.fields.len());
-	let mut load_struct = Vec::with_capacity(input.fields.len());
-	for field in input.fields {
-		let typ = field.typ;
-		let name = field.name;
-		let squares = field.squares;
+				load.push(if let Some(squares) = field.squares { quote!{
+					let #name = {
+						let mut vec = Vec::with_capacity((#squares) as usize);
+						for _ in 0..((#squares) as usize) {
+							vec.push(#typ::load(reader)?);
+						}
+						vec
+					};
+				}} else { quote!{
+					let #name = #typ::load(reader)?;
+				}});
 
-		fields.push( if let Some(_) = field.type_width {
-			quote! {
-				pub #name: #typ,
+				load_struct.push(quote!{
+					#name,
+				})
 			}
-		} else {
-			quote! {
-				pub #name: Vec<#typ>,
-			}
-		});
 
-		load.push(if let Some(n) = field.type_width {
-			quote! {
-				let #name = {
-					let mut buf = [0u8; #n];
-					let length = reader.read(&mut buf)?;
-					if length == #n {
-						#typ::from_be_bytes(buf)
-					} else {
-						Err(Error::ReadWrongAmountOfData { actual: length, expected: #n, string: stringify!(#name)})?
-					}
-				};
-			}
-		} else {
-			quote! {
-				let #name = {
-					let mut vec = Vec::with_capacity((#squares) as usize);
-					for _ in 0..((#squares) as usize) {
-						vec.push(#typ::load(reader)?);
-					}
-					vec
-				};
-			}
-		});
-		load_struct.push(
-			quote! {
-				#name,
-			}
-		)
-	}
-
-	let exp = quote! {
-		#[derive(Debug)]
-		struct #name {
-			#( #fields )*
-		}
-
-		impl Load for #name {
-			fn load<R: Read>(reader: &mut R) -> Result<#name, Error> {
+			let implementation = if let Some(braces) = braces { quote! {
+				#braces
+			} } else { quote!{
 				#( #load )*
-				Ok( Self {
+				Ok(Self {
 					#( #load_struct )*
-				} )
-			}
-		}
-	};
+				})
+			}};
 
-	exp.into()
+			let exp = quote!{
+				#[derive(Debug)]
+				pub struct #name {
+					#( #field_declarations )*
+				}
+
+				impl Load for #name {
+					fn load<R: Read>(reader: &mut R) -> Result<#name, Error> {
+						#implementation
+					}
+				}
+			};
+
+			exp.into()
+		},
+		Declaration::Enum {
+			name,
+			tag_type: SpecialType(tag_type),
+			variants
+		} => {
+			let mut variants_out = Vec::with_capacity(variants.len());
+			let mut variants_match = Vec::with_capacity(variants.len());
+
+			for variant in variants {
+				let name = variant.name;
+				let index = variant.index;
+
+				let mut field_declarations = Vec::with_capacity(variant.fields.len());
+				let mut field_load = Vec::with_capacity(variant.fields.len());
+				let mut field_load_struct = Vec::with_capacity(variant.fields.len());
+
+				for field in variant.fields {
+					let typ = field.typ.0;
+					let name = field.name;
+
+					field_declarations.push(if field.squares.is_some() { quote! {
+						#name: Vec<#typ>,
+					}} else { quote! {
+						#name: #typ,
+					}});
+
+					field_load.push(if let Some(squares) = field.squares { quote!{
+						let #name = {
+							let mut vec = Vec::with_capacity((#squares) as usize);
+							for _ in 0..((#squares) as usize) {
+								vec.push(#typ::load(reader)?);
+							}
+							vec
+						};
+					}} else { quote! {
+						let #name = #typ::load(reader)?;
+					}});
+
+					field_load_struct.push(quote!{
+						#name,
+					});
+				}
+
+				variants_out.push( quote!{
+					#name { #( #field_declarations )* },
+				});
+
+				variants_match.push(quote!{
+					#index => {
+						#( #field_load )*
+						Ok( Self::#name {
+							#( #field_load_struct )*
+						})
+					},
+				});
+			}
+
+			let exp = quote!{
+				#[derive(Debug)]
+				pub enum #name {
+					#( #variants_out )*
+				}
+				impl Load for #name {
+					fn load<R: Read>(reader: &mut R) -> Result<#name, Error> {
+						let tag = #tag_type::load(reader)?;
+
+						match tag {
+							#( #variants_match )*
+							_ => Err(Error::ReadWrongTag { actual: tag as u32, string: stringify!(tag: #tag_type) })
+						}
+					}
+				}
+			};
+
+			exp.into()
+		},
+	}
 }
--- jvm/src/classfile.rs
+++ jvm/src/classfile.rs
@@ -4,8 +4,10 @@ use gen::declare_jvm_struct;
 
 #[derive(Debug)]
 enum Error {
-	ReadWrongAmountOfData{ actual: usize, expected: usize, string: &'static str},
-	IoError(std::io::Error)
+	ReadWrongAmountOfDataStr { actual: usize, expected: usize, string: &'static str },
+	ReadWrongAmountOfData { actual: usize, expected: usize },
+	ReadWrongTag { actual: u32, string: &'static str },
+	IoError(std::io::Error),
 }
 
 impl From<std::io::Error> for Error {
@@ -18,15 +20,81 @@ trait Load {
 	fn load<R: Read>(reader: &mut R) -> Result<Self, Error> where Self: Sized;
 }
 
-declare_jvm_struct!(
-	CpInfo {
-	    u1 tag;
-	    u1 info[];
+macro_rules! impl_load_for {
+	($t:ty, $n:literal) => {
+		impl Load for $t {
+			fn load<R: Read>(reader: &mut R) -> Result<Self, Error> where Self: Sized {
+				let mut buf = [0u8; $n];
+				let length = reader.read(&mut buf)?;
+				if length == $n {
+					Ok(<$t>::from_be_bytes(buf))
+				} else {
+					Err(Error::ReadWrongAmountOfDataStr { actual: length, expected: $n, string: stringify!($t) })
+				}
+			}
+		}
+	}
+}
+
+impl_load_for!(u8, 1);
+impl_load_for!(u16, 2);
+impl_load_for!(u32, 4);
+
+declare_jvm_struct!( // 4.4, Table 4.3
+	enum CpInfo (u1) {
+		7 = Class {
+    		u2 name_index;
+		},
+		9 = FieldRef {
+			u2 class_index;
+			u2 name_and_type_index;
+		},
+		10 = MethodRef {
+			u2 class_index;
+			u2 name_and_type_index;
+		},
+		11 = InterfaceMethodRef {
+			u2 class_index;
+			u2 name_and_type_index;
+		},
+		8 = String {
+			u2 string_index;
+		},
+		3 = Integer {
+			u4 bytes;
+		},
+		4 = Float {
+			u4 bytes;
+		},
+		5 = Long {
+			u4 high_bytes;
+			u4 low_bytes;
+		},
+		6 = Double {
+			u4 high_bytes;
+			u4 low_bytes;
+		},
+		12 = NameAndType {
+			u2 name_index;
+			u2 descriptor_index;
+		},
+		1 = Utf8 {
+			u2 length;
+			u1 bytes[length];
+		},
+		15 = MethodHandle {
+			u1 reference_kind;
+			u2 reference_index;
+		},
+		18 = InvokeDynamic {
+			u2 bootstrap_method_attr_index;
+			u2 name_and_type_index;
+		},
 	}
 );
 
-declare_jvm_struct!(
-	FieldInfo {
+declare_jvm_struct!( // 4.5
+	struct FieldInfo {
 	    u2             access_flags;
 	    u2             name_index;
 	    u2             descriptor_index;
@@ -35,16 +103,16 @@ declare_jvm_struct!(
 	}
 );
 
-declare_jvm_struct!(
-	AttributeInfo {
+declare_jvm_struct!( // 4.7
+	struct AttributeInfo {
 	    u2 attribute_name_index;
 	    u4 attribute_length;
 	    u1 info[attribute_length];
 	}
 );
 
-declare_jvm_struct!(
-	MethodInfo {
+declare_jvm_struct!( // 4.6
+	struct MethodInfo {
 	    u2             access_flags;
 	    u2             name_index;
 	    u2             descriptor_index;
@@ -53,8 +121,8 @@ declare_jvm_struct!(
 	}
 );
 
-declare_jvm_struct!(
-	ClassFile {
+declare_jvm_struct!( // 4.1
+	struct ClassFile {
 	    u4             magic;
 	    u2             minor_version;
 	    u2             major_version;
@@ -79,7 +147,7 @@ mod testing {
 	use super::{ClassFile, Load};
 	#[test]
 	fn try_parse_classfile() {
-		let bytes = include_bytes!("../../java_example_classfiles/Test.java");
+		let bytes = include_bytes!("../../java_example_classfiles/Test.class");
 		let classfile = ClassFile::load(&mut &bytes[..]).unwrap();
 		dbg!(classfile);
 	}
