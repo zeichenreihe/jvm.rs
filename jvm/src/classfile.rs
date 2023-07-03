bfafd8ec342d6a645a96fe3c549cb5160606d62a
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-07-03 23:20:27 +0200

    impl the rest of the class file reading
    - this however doesn't check if the format is 100% correct

--- jvm/src/classfile.rs
+++ jvm/src/classfile.rs
@@ -19,6 +19,8 @@ pub enum Error {
 	InvalidConstantPoolTag ( u8 ),
 	NoUtf8AtAddress ( u16 ),
 	NoCodeAttribute,
+	InvalidAnnotationElementValueTag(char),
+	FutureUsed(u8),
 	WrongConstantPoolTag,
 	ReadDifferentValueThanExpected(String),
 }
@@ -307,8 +309,29 @@ declare_jvm_struct!( // 4.5
 	}
 );
 
+declare_jvm_struct!( // 4.7.2
+	struct ConstantValueAttribute {
+		u4 attribute_length = 2;
+		u2 constantvalue_index;
+	}
+);
+
 declare_jvm_struct!( // 4.7.3
-	struct ExceptionTableElement {
+	struct CodeAttribute {
+		u4 attribute_length;
+		u2 max_stack;
+		u2 max_locals;
+		u4 code_length;
+		u1 code[code_length];
+		u2 exception_table_length;
+		ExceptionTableEntry exception_table[exception_table_length];
+		u2 attributes_count;
+		AttributeInfo attributes[attributes_count];
+	}
+);
+
+declare_jvm_struct!( // 4.7.3, exception_table
+	struct ExceptionTableEntry {
 		u2 start_pc;
 		u2 end_pc;
 		u2 handler_pc;
@@ -316,8 +339,133 @@ declare_jvm_struct!( // 4.7.3
 	}
 );
 
+declare_jvm_struct!( // 4.7.4
+	struct StackMapTableAttribute {
+		u4 attribute_length;
+		u2 number_of_entries;
+		StackMapFrame entries[number_of_entries];
+	}
+);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum VerificationTypeInfo {
+	Top, Integer, Float, Long, Double, Null, UninitializedThis,
+	Object {
+		constant_pool_index: u16,
+	},
+	Uninitialized {
+		offset: u16,
+	},
+}
+
+impl<R: Read> Parse<R> for VerificationTypeInfo {
+	fn parse(reader: &mut R, constant_pool: Option<&Vec<CpInfo>>) -> Result<Self, Error> where Self: Sized {
+		let tag = u8::parse(reader, constant_pool)?;
+
+		match tag {
+			0 => Ok(Self::Top),
+			1 => Ok(Self::Integer),
+			2 => Ok(Self::Float),
+			3 => Ok(Self::Double),
+			4 => Ok(Self::Long),
+			5 => Ok(Self::Null),
+			6 => Ok(Self::UninitializedThis),
+			7 => Ok(Self::Object {
+				constant_pool_index: u16::parse(reader, constant_pool)?,
+			}),
+			8 => Ok(Self::Uninitialized {
+				offset: u16::parse(reader, constant_pool)?,
+			}),
+			_ => Err(Error::FutureUsed(tag)),
+		}
+	}
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum StackMapFrame {
+	Same {
+		offset_delta: u16,
+	},
+	SameLocals1StackItem {
+		offset_delta: u16,
+		stack: VerificationTypeInfo,
+	},
+	Chop {
+		offset_delta: u16,
+		k: u8,
+	},
+	Append {
+		offset_delta: u16,
+		locals: Vec<VerificationTypeInfo>,
+	},
+	Full {
+		offset_delta: u16,
+		number_of_locals: u16,
+		locals: Vec<VerificationTypeInfo>, // [number_of_locals]
+		number_of_stack_items: u16,
+		stack: Vec<VerificationTypeInfo>, // [number_of_stack_items]
+	}
+}
+
+impl<R: Read> Parse<R> for StackMapFrame {
+	fn parse(reader: &mut R, constant_pool: Option<&Vec<CpInfo>>) -> Result<Self, Error> where Self: Sized {
+		let frame_type = u8::parse(reader, constant_pool)?;
+
+		match frame_type {
+			offset_delta @ 0..=63 => Ok(Self::Same {
+				offset_delta: offset_delta as u16,
+			}),
+			frame_type @ 64..=127 => Ok(Self::SameLocals1StackItem {
+				offset_delta: frame_type as u16 - 64,
+				stack: VerificationTypeInfo::parse(reader, constant_pool)?,
+			}),
+			128..=246 => Err(Error::FutureUsed(frame_type)),
+			247 => Ok(Self::SameLocals1StackItem {
+				offset_delta: u16::parse(reader, constant_pool)?,
+				stack: VerificationTypeInfo::parse(reader, constant_pool)?,
+			}),
+			frame_type @ 248..=250 => Ok(Self::Chop {
+				offset_delta: u16::parse(reader, constant_pool)?,
+				k: 251 - frame_type,
+			}),
+			251 => Ok(Self::Same {
+				offset_delta: u16::parse(reader, constant_pool)?,
+			}),
+			frame_type @ 252..=254 => Ok(Self::Append {
+				offset_delta: u16::parse(reader, constant_pool)?,
+				locals: Vec::parse_multi(reader, constant_pool, (frame_type - 251) as usize)?,
+			}),
+			255 => {
+				let offset_delta = u16::parse(reader, constant_pool)?;
+				let number_of_locals = u16::parse(reader, constant_pool)?;
+				let locals = Vec::parse_multi(reader, constant_pool, number_of_locals as usize)?;
+				let number_of_stack_items = u16::parse(reader, constant_pool)?;
+				let stack = Vec::parse_multi(reader, constant_pool, number_of_stack_items as usize)?;
+
+				Ok(Self::Full { offset_delta, number_of_locals, locals, number_of_stack_items, stack })
+			},
+		}
+	}
+}
+
+declare_jvm_struct!( // 4.7.5
+	struct ExceptionsAttribute {
+		u4 attribute_length;
+		u2 number_of_exceptions;
+		u2 exception_index_table[number_of_exceptions];
+	}
+);
+
 declare_jvm_struct!( // 4.7.6
-	struct AttributeInnerClassesInnerStruct {
+	struct InnerClassesAttribute {
+		u4 attribute_length;
+		u2 number_of_classes;
+		InnerClassesAttributeClassesElement classes[number_of_classes];
+	}
+);
+
+declare_jvm_struct!( // 4.7.6, classes
+	struct InnerClassesAttributeClassesElement {
 		u2 inner_class_info_index;
 		u2 outer_class_info_index;
 		u2 inner_name_index;
@@ -361,14 +509,30 @@ declare_jvm_struct!( // 4.7.11
 );
 
 declare_jvm_struct!( // 4.7.12
-	struct AttributeLineNumberTableInnerStruct {
+	struct LineNumberTableAttribute {
+		u4 attribute_length;
+		u2 line_number_table_length;
+		LineNumberTableEntry line_number_table[line_number_table_length];
+	}
+);
+
+declare_jvm_struct!( // 4.7.12, line_number_table
+	struct LineNumberTableEntry {
 		u2 start_pc;
 		u2 line_number;
 	}
 );
 
 declare_jvm_struct!( // 4.7.13
-	struct AttributeLocalVariableTableInnerStruct {
+	struct LocalVariableTableAttribute {
+		u4 attribute_length;
+		u2 local_variable_table_length;
+		LocalVariableTableEntry local_variable_table[local_variable_table_length];
+	}
+);
+
+declare_jvm_struct!( // 4.7.13, local_variable_table
+	struct LocalVariableTableEntry {
 		u2 start_pc;
 		u2 length;
 		u2 name_index;
@@ -378,7 +542,15 @@ declare_jvm_struct!( // 4.7.13
 );
 
 declare_jvm_struct!( // 4.7.14
-	struct AttributeLocalVariableTypeTable {
+	struct LocalVariableTypeTableAttribute {
+		u4 attribute_length;
+		u2 local_variable_type_table_length;
+		LocalVariableTypeTableEntry local_variable_type_table[local_variable_type_table_length];
+	}
+);
+
+declare_jvm_struct!( // 4.7.14, local_variable_type_table
+	struct LocalVariableTypeTableEntry {
 		u2 start_pc;
 		u2 length;
 		u2 name_index;
@@ -387,8 +559,134 @@ declare_jvm_struct!( // 4.7.14
 	}
 );
 
+declare_jvm_struct!( // 4.7.15
+	struct DeprecatedAttribute {
+		u4 attribute_length = 0;
+	}
+);
+
+declare_jvm_struct!( // 4.7.16
+	struct RuntimeVisibleAnnotationsAttribute {
+		u4         attribute_length;
+		u2         num_annotations;
+		Annotation annotations[num_annotations];
+	}
+);
+
+declare_jvm_struct!( // 4.7.16, annotations
+	struct Annotation {
+		u2 type_index;
+		u2 num_element_value_pairs;
+		AnnotationElementValuePair element_value_pairs[num_element_value_pairs];
+	}
+);
+
+declare_jvm_struct!( // 4.7.16, element_value_pairs
+	struct AnnotationElementValuePair {
+		u2            element_name_index;
+		AnnotationElementValue value;
+	}
+);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum AnnotationElementValue { // 4.7.16.1, value
+	ConstantValueIndex { // B, C, D, F, I, J, S, Z, s
+		const_value_index: u16,
+	},
+	EnumConstValue { // e
+		type_name_index: u16,
+		const_name_index: u16,
+	},
+	ClassInfoIndex { // c
+		class_info_index: u16,
+	},
+	AnnotationValue { // @
+		annotation_value: Annotation,
+	},
+	ArrayValue { // [
+		num_values: u16,
+		values: Vec<AnnotationElementValue>, // [num_values]
+	}
+}
+
+impl<R: Read> Parse<R> for AnnotationElementValue {
+	fn parse(reader: &mut R, constant_pool: Option<&Vec<CpInfo>>) -> Result<Self, Error> where Self: Sized {
+		let tag = u8::parse(reader, constant_pool)?;
+
+		let tag = tag.try_into().unwrap(); // TODO: fix this, this should be handled gracefully!
+
+		Ok(match tag {
+			'B' | 'C' | 'D' | 'F' | 'I' | 'J' | 'S' | 'Z' | 's' => Self::ConstantValueIndex {
+				const_value_index: u16::parse(reader, constant_pool)?,
+			},
+			'e' => Self::EnumConstValue {
+				type_name_index: u16::parse(reader, constant_pool)?,
+				const_name_index: u16::parse(reader, constant_pool)?,
+			},
+			'c' => Self::ClassInfoIndex {
+				class_info_index: u16::parse(reader, constant_pool)?,
+			},
+			'@' => Self::AnnotationValue {
+				annotation_value: Annotation::parse(reader, constant_pool)?,
+			},
+			'[' => {
+				let num_values = u16::parse(reader, constant_pool)?;
+				let values = Vec::parse_multi(reader, constant_pool, num_values as usize)?;
+				Self::ArrayValue { num_values, values }
+			},
+			_ => Err(Error::InvalidAnnotationElementValueTag(tag))?
+		})
+	}
+}
+
+declare_jvm_struct!( // 4.7.17
+	struct RuntimeInvisibleAnnotationsAttribute {
+		u4         attribute_length;
+		u2         num_annotations;
+		Annotation annotations[num_annotations];
+	}
+);
+
+declare_jvm_struct!( // 4.7.18
+	struct RuntimeVisibleParameterAnnotationsAttribute {
+		u4 attribute_length;
+		u1 num_parameters;
+		ParameterAnnotationPair parameter_annotations[num_parameters];
+	}
+);
+
+declare_jvm_struct!( // 4.7.18, parameter_annotations
+	struct ParameterAnnotationPair {
+		u2         num_annotations;
+		Annotation annotations[num_annotations];
+	}
+);
+
+declare_jvm_struct!( // 4.7.19
+	struct RuntimeInvisibleParameterAnnotationsAttribute {
+		u4 attribute_length;
+		u1 num_parameters;
+		ParameterAnnotationPair parameter_annotations[num_parameters];
+	}
+);
+
+declare_jvm_struct!( // 4.7.20
+	struct AnnotationDefaultAttribute {
+		u4            attribute_length;
+    	AnnotationElementValue default_value;
+	}
+);
+
 declare_jvm_struct!( // 4.7.21
-	struct AttributeBootstrapMethodsInnerStruct {
+	struct BootstrapMethodsAttribute {
+		u4 attribute_length;
+		u2 num_bootstrap_methods;
+		BootstrapMethodsAttributeEntry bootstrap_methods[num_bootstrap_methods];
+	}
+);
+
+declare_jvm_struct!( // 4.7.21, bootstrap_methods
+	struct BootstrapMethodsAttributeEntry {
 		u2 bootstrap_method_ref;
 		u2 num_bootstrap_arguments;
 		u2 bootstrap_arguments[num_bootstrap_arguments];
@@ -396,82 +694,27 @@ declare_jvm_struct!( // 4.7.21
 );
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct StackMapFrame(); // TODO: fill
-
-//#[non_exhaustive]
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum AttributeInfo { // 4.7
-	ConstantValue { // 1.0.2, 45.3
-		constantvalue_index: u16,
-	},
-	Code { // 1.0.2, 45.3
-		max_stack: u16,
-		max_locals: u16,
-		code_length: u32,
-		code: Vec<u8>, //[code_length];
-		exception_table_length: u16,
-		exception_table: Vec<ExceptionTableElement>, // [exception_table_length]
-		attributes_count: u16,
-		attributes: Vec<AttributeInfo>, // [attributes_count]
-	},
-	StackMapTable { // 6, 50.0
-		number_of_entries: u16,
-		entries: Vec<StackMapFrame>, // [number_of_entries]
-	},
-	Exceptions { // 1.0.2, 45.3
-		number_of_exceptions: u16,
-		exception_index_table: Vec<u16>, // [number_of_exceptions]
-	},
-	InnerClasses { // 1.1, 45.3
-		number_of_classes: u16,
-		classes: Vec<AttributeInnerClassesInnerStruct>, // [number_of_classes]
-	},
-	EnclosingMethod { // 5.0, 49.0
-		class_index: u16,
-		method_index: u16,
-	},
-	Synthetic { }, // 1.1, 45.3
-	Signature { // 5.0, 49.0
-		signature_index: u16,
-	},
-	SourceFile { // 1.0.2, 45.3
-		sourcefile_index: u16,
-	},
-	SourceDebugExtension { // 5.0, 49.0
-		debug_extension: Vec<u8>, // [attribute_length]
-	},
-	LineNumberTable { // 1.0.2, 45.3
-		line_number_table_length: u16,
-		line_number_table: Vec<AttributeLineNumberTableInnerStruct>, // [line_number_table_length]
-	},
-	LocalVariableTable { // 1.0.2, 45.3
-		local_variable_table_length: u16,
-		local_variable_table: Vec<AttributeLocalVariableTableInnerStruct>, // [local_variable_table_length]
-	},
-	LocalVariableTypeTable { // 5.0, 49.0
-		local_variable_type_table_length: u16,
-		local_variable_type_table: Vec<AttributeLocalVariableTypeTable>, // [local_variable_type_table_length]
-	},
-	Deprecated { }, // 1.1, 45.3
-	RuntimeVisibleAnnotations { // 5.0, 49.0
-		// TODO
-	},
-	RuntimeInvisibleAnnotations { // 5.0, 49.0
-		// TODO
-	},
-	RuntimeVisibleParameterAnnotations { // 5.0, 49.0
-		// TODO
-	},
-	RuntimeInvisibleParameterAnnotations { // 5.0, 49.0
-		// TODO
-	},
-	AnnotationDefault { // 5.0, 49.0
-		// TODO
-	},
-	BootstrapMethods { // 7, 51.0
-		num_bootstrap_methods: u16,
-		bootstrap_methods: Vec<AttributeBootstrapMethodsInnerStruct>, // [num_bootstrap_methods]
-	},
+	ConstantValue(ConstantValueAttribute), // 1.0.2, 45.3
+	Code(CodeAttribute), // 1.0.2, 45.3
+	StackMapTable(StackMapTableAttribute), // 6, 50.0
+	Exceptions(ExceptionsAttribute), // 1.0.2, 45.3
+	InnerClasses(InnerClassesAttribute), // 1.1, 45.3
+	EnclosingMethod(EnclosingMethodAttribute), // 5.0, 49.0
+	Synthetic(SyntheticAttribute), // 1.1, 45.3
+	Signature(SignatureAttribute), // 5.0, 49.0
+	SourceFile(SourceFileAttribute), // 1.0.2, 45.3
+	SourceDebugExtension(SourceDebugExtensionsAttribute), // 5.0, 49.0
+	LineNumberTable(LineNumberTableAttribute), // 1.0.2, 45.3
+	LocalVariableTable(LocalVariableTableAttribute), // 1.0.2, 45.3
+	LocalVariableTypeTable(LocalVariableTypeTableAttribute), // 5.0, 49.0
+	Deprecated(DeprecatedAttribute), // 1.1, 45.3
+	RuntimeVisibleAnnotations(RuntimeVisibleAnnotationsAttribute), // 5.0, 49.0
+	RuntimeInvisibleAnnotations(RuntimeInvisibleAnnotationsAttribute), // 5.0, 49.0
+	RuntimeVisibleParameterAnnotations(RuntimeVisibleParameterAnnotationsAttribute), // 5.0, 49.0
+	RuntimeInvisibleParameterAnnotations(RuntimeInvisibleParameterAnnotationsAttribute), // 5.0, 49.0
+	AnnotationDefault(AnnotationDefaultAttribute), // 5.0, 49.0
+	BootstrapMethods(BootstrapMethodsAttribute), // 7, 51.0
 	Unknown {
 		attribute_name_index: u16,
 		attribute_length: u32,
@@ -484,7 +727,6 @@ impl<R: Read> Parse<R> for AttributeInfo {
 		let pool = constant_pool.expect("Cannot parse AttributeInfo without a constant pool, this should not be reachable!");
 
 		let attribute_name_index = u16::parse(reader, constant_pool)?;
-		let attribute_length = u32::parse(reader, constant_pool)?;
 		let attribute_name = pool.get(attribute_name_index as usize - 1)
 			.ok_or_else(|| Error::InvalidConstantPoolAddress {
 				address: attribute_name_index,
@@ -496,42 +738,32 @@ impl<R: Read> Parse<R> for AttributeInfo {
 			Err(Error::NoUtf8AtAddress(attribute_name_index))?
 		};
 
-		Ok(match (attribute_length, s) {
-			(2, "ConstantValue") => Self::ConstantValue {
-				constantvalue_index: u16::parse(reader, constant_pool)?,
-			},
-			(_, "Code") => {
-				let max_stack = u16::parse(reader, constant_pool)?;
-				let max_locals = u16::parse(reader, constant_pool)?;
-				let code_length = u32::parse(reader, constant_pool)?;
-				let code = Vec::parse_multi(reader, constant_pool, code_length as usize)?;
-				let exception_table_length = u16::parse(reader, constant_pool)?;
-				let exception_table = Vec::parse_multi(reader, constant_pool, exception_table_length as usize)?;
-				let attributes_count = u16::parse(reader, constant_pool)?;
-				let attributes = Vec::parse_multi(reader, constant_pool, attributes_count as usize)?;
-				Self::Code {
-					max_stack, max_locals, code_length, code, exception_table_length, exception_table, attributes_count, attributes
-				}
-			},
-			(2, "SourceFile") => Self::SourceFile { sourcefile_index: u16::parse(reader, constant_pool)? },
-			(_, "LineNumberTable") => {
-				let line_number_table_length = u16::parse(reader, constant_pool)?;
-				let line_number_table = Vec::parse_multi(reader, constant_pool, line_number_table_length as usize)?;
-				Self::LineNumberTable {
-					line_number_table_length, line_number_table
-				}
-			},
-			(0, "Deprecated") => Self::Deprecated {},
-			_ => Self::Unknown {
-				attribute_name_index, attribute_length,
-				info: {
-					println!("unknown attr: {s}");
-					let mut vec = Vec::with_capacity(attribute_length as usize);
-					for _ in 0..(attribute_length as usize) {
-						vec.push(u8::parse(reader, constant_pool)?);
-					}
-					vec
-				}
+		Ok(match s {
+			"ConstantValue" => Self::ConstantValue(ConstantValueAttribute::parse(reader, constant_pool)?),
+			"Code" => Self::Code(CodeAttribute::parse(reader, constant_pool)?),
+			"StackMapTable" => Self::StackMapTable(StackMapTableAttribute::parse(reader, constant_pool)?),
+			"Exceptions" => Self::Exceptions(ExceptionsAttribute::parse(reader, constant_pool)?),
+			"InnerClasses" => Self::InnerClasses(InnerClassesAttribute::parse(reader, constant_pool)?),
+			"EnclosingMethod" => Self::EnclosingMethod(EnclosingMethodAttribute::parse(reader, constant_pool)?),
+			"Synthetic" => Self::Synthetic(SyntheticAttribute::parse(reader, constant_pool)?),
+			"Signature" => Self::Signature(SignatureAttribute::parse(reader, constant_pool)?),
+			"SourceFile" => Self::SourceFile(SourceFileAttribute::parse(reader, constant_pool)?),
+			"SourceDebugExtension" => Self::SourceDebugExtension(SourceDebugExtensionsAttribute::parse(reader, constant_pool)?),
+			"LineNumberTable" => Self::LineNumberTable(LineNumberTableAttribute::parse(reader, constant_pool)?),
+			"LocalVariableTable" => Self::LocalVariableTable(LocalVariableTableAttribute::parse(reader, constant_pool)?),
+			"LocalVariableTypeTable" => Self::LocalVariableTypeTable(LocalVariableTypeTableAttribute::parse(reader, constant_pool)?),
+			"Deprecated" => Self::Deprecated(DeprecatedAttribute::parse(reader, constant_pool)?),
+			"RuntimeVisibleAnnotations" => Self::RuntimeVisibleAnnotations(RuntimeVisibleAnnotationsAttribute::parse(reader, constant_pool)?),
+			"RuntimeInvisibleAnnotations" => Self::RuntimeInvisibleAnnotations(RuntimeInvisibleAnnotationsAttribute::parse(reader, constant_pool)?),
+			"RuntimeVisibleParameterAnnotations" => Self::RuntimeVisibleParameterAnnotations(RuntimeVisibleParameterAnnotationsAttribute::parse(reader, constant_pool)?),
+			"RuntimeInvisibleParameterAnnotations" => Self::RuntimeInvisibleParameterAnnotations(RuntimeInvisibleParameterAnnotationsAttribute::parse(reader, constant_pool)?),
+			"AnnotationDefault" => Self::AnnotationDefault(AnnotationDefaultAttribute::parse(reader, constant_pool)?),
+			"BootstrapMethods" => Self::BootstrapMethods(BootstrapMethodsAttribute::parse(reader, constant_pool)?),
+			_ => {
+				let attribute_length = u32::parse(reader, constant_pool)?;
+				let info = Vec::parse_multi(reader, constant_pool, attribute_length as usize)?;
+				eprintln!("WARN: unknown attr: {s}: {info:?}");
+				Self::Unknown { attribute_name_index, attribute_length, info }
 			},
 		})
 	}
