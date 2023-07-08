956370be5976e602a0577dac7b7a82612622496b
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-07-08 23:07:03 +0200

    improve `errors.rs`: `impl Error for ...`

--- jvm/src/errors.rs
+++ jvm/src/errors.rs
@@ -1,7 +1,28 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 use crate::classfile::JUtf8;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct OutOfBoundsError;
+
+impl Display for OutOfBoundsError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		f.write_str("out of bounds.")
+	}
+}
+
+impl Error for OutOfBoundsError {}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ConstantPoolTagMismatchError;
+
+impl Display for ConstantPoolTagMismatchError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		f.write_str("constant pool tag not defined")
+	}
+}
+
+impl Error for ConstantPoolTagMismatchError {}
 
 #[derive(Debug)]
 pub enum ClassFileParseError {
@@ -21,6 +42,14 @@ pub enum ClassFileParseError {
 	IoError(std::io::Error),
 }
 
+impl Display for ClassFileParseError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{self:?}")
+	}
+}
+
+impl Error for ClassFileParseError {}
+
 impl From<std::io::Error> for ClassFileParseError {
 	fn from(value: std::io::Error) -> Self {
 		Self::IoError(value)
@@ -28,13 +57,19 @@ impl From<std::io::Error> for ClassFileParseError {
 }
 
 impl From<std::str::Utf8Error> for ClassFileParseError {
-	fn from(value: std::str::Utf8Error) -> Self {
+	fn from(_value: std::str::Utf8Error) -> Self {
 		todo!()
 	}
 }
 
 impl From<std::string::FromUtf8Error> for ClassFileParseError {
-	fn from(value: std::string::FromUtf8Error) -> Self {
+	fn from(_value: std::string::FromUtf8Error) -> Self {
+		todo!()
+	}
+}
+
+impl From<ConstantPoolTagMismatchError> for ClassFileParseError {
+	fn from(_value: ConstantPoolTagMismatchError) -> Self {
 		todo!()
 	}
 }
@@ -47,6 +82,19 @@ pub enum RuntimeError {
 	OutOfBounds(OutOfBoundsError),
 }
 
+impl Display for RuntimeError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::NoClassDefFound(jutf8) => write!(f, "no class found: {jutf8:?}"),
+			Self::ClassLoad(e) => write!(f, "while loading a class: {e:?}"),
+			Self::ClassAlreadyLoaded(jutf8) => write!(f, "class already loaded: {jutf8:?}"),
+			Self::OutOfBounds(e) => write!(f, "out of bounds: {e:?}"),
+		}
+	}
+}
+
+impl Error for RuntimeError {}
+
 impl From<ClassFileParseError> for RuntimeError {
 	fn from(value: ClassFileParseError) -> Self {
 		Self::ClassLoad(value)
@@ -54,7 +102,7 @@ impl From<ClassFileParseError> for RuntimeError {
 }
 
 impl From<OutOfBoundsError> for RuntimeError {
-	fn from(value: OutOfBoundsError) -> Self {
+	fn from(_: OutOfBoundsError) -> Self {
 		Self::OutOfBounds(OutOfBoundsError)
 	}
 }
